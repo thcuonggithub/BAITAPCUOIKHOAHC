a56b3beaae26a6b2011a3ac0efb25b6739cd3d74
thcuonggithub <166672916+thcuonggithub@users.noreply.github.com>
2024-05-05 21:24:55 +0700

    Update style.js

--- style.js
+++ style.js
@@ -20,8 +20,8 @@ $(document).ready(function() {
         arrows: false,
         autoplay: true,
         autoplaySpeed: 30000,
-        // prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-        // nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
         dots: true,
     });
 });
